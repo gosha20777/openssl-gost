e2f8baeef8cf3d00b79b2ad15601f2be88f21ad0
gosha20777 <gosha20777@live.ru>
2019-11-21 16:55:30 +0300

    feat:upd nginx

--- nginx-gost/gen_demo_gost_certs.sh
+++ nginx-gost/gen_demo_gost_certs.sh
@@ -6,7 +6,7 @@ openssl req -x509 -newkey gost2012_256 -pkeyopt paramset:A -nodes -days 10000 -k
 openssl genpkey -algorithm gost2012_256 -pkeyopt paramset:A -out /etc/nginx/key.pem
 
 # Generate new request for site
-openssl req -new -key /etc/nginx/gost.example.com.key -out /etc/nginx/gost.example.com.csr -subj "/C=RU/L=Kemerovo/O=My site with GOST/CN=gost.example.com"
+openssl req -new -key /etc/nginx/key.pem -out /etc/nginx/gost.example.com.csr -subj "/C=RU/L=Kemerovo/O=My site with GOST/CN=gost.example.com"
 
 # Sign request with CA
 openssl x509 -req -in /etc/nginx/gost.example.com.csr -CA /etc/nginx/ca_cert.crt -CAkey /etc/nginx/ca_key.pem -CAcreateserial -out /etc/nginx/cert.pem -days 5000
